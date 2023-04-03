5b6ad940283b3d679cbb6c3b0af793a8a88cccbb
chulleyboy <ryanmmassenburg@gmail.com>
2023-04-03 14:23:26 -0700

    added user questions for creating the readme

--- index.js
+++ index.js
@@ -1,7 +1,56 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const generateMarkdown=require("./utils/generateMarkdown");
+const { default: Choice } = require('inquirer/lib/objects/choice');
+
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+{
+	type:"input",
+	name:"title",
+	message:"Enter a name for the project?"
+},
+{
+	type:"input",
+	name:"description",
+	message:"Enter a description of the project"
+},
+{
+	type:"input",
+	name:"installation",
+	message:"Enter installation instruction for the project"
+},{
+	type:"input",
+	name:"usage",
+	message:"Enter usage instructions for the project"
+},
+{
+	type:"input",
+	name:"contribution",
+	message:"Enter contribution guidelines for the project"
+},{
+	type:"input",
+	name:"tests",
+	message:"Enter information about tests for the project"
+},
+{
+	type:"list",
+	name:"license",
+	message:"Choose the type of liscense the project will use",
+	choices: ["MIT", "Apache 2.0", "BSD 3-Clause", "GNU General Public 2.0", "none"]
+},
+{
+	type:"input",
+	name:"username",
+	message:"Enter your GitHub username"
+},
+{
+	type:"input",
+	name:"email",
+	message:"Enter your email address"
+},
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
