8f3eb9c977ae867cea422b6ed0dcd08c504be9f1
chulleyboy <ryanmmassenburg@gmail.com>
2023-04-05 15:45:31 -0700

    fixed license not being added properly, modified formatting

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -2,9 +2,9 @@
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
   if (license !== "none"){
-    return `![GitHub license](https://img.shields.io/badge/license-${license}-blue.svg)`
+    return `![GitHub license](https://img.shields.io/badge/license-${license}-blue.svg)`;
   }
-  return""
+  return"";
 }
 
 // TODO: Create a function that returns the license link
@@ -15,49 +15,44 @@ function renderLicenseLink(license) {}
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
   if (license !== "none"){
-    return 
-    `## License /n
-    /n
-    ${data.license}/n`
+    return `## License \n\n ${license}`;
   }
-  return""
+  return"";
 }
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `
-${renderLicenseBadge(data.license)} /n
-# ${data.title} /n
-/n
-## Description /n
-/n
-${data.description} /n
-/n
-## Installation /n
-/n
-${data.installation} /n
-/n
-## Usage /n
-/n
-${data.usage} /n
-/n
-## Contribution Guidelines /n
-/n
-${data.contribution} /n
-/n
-## Tests /n
-/n
-${data.tests} /n
-/n
-## Questions /n
-/n
-Contact me by my email or github if you have additional questions /n
-${data.email} /n
-https://github.com/${data.username} /n
-/n
-${renderLicenseSection(data.license)}
+${renderLicenseBadge(data.license)}
+# ${data.title}
 
-`;
+## Description
+
+${data.description}
+
+## Installation
+
+${data.installation}
+
+## Usage
+
+${data.usage}
+
+## Contribution Guidelines
+
+${data.contribution}
+
+## Tests
+
+${data.tests}
+
+## Questions
+
+Contact me by my email or github if you have additional questions
+${data.email}
+https://github.com/${data.username}
+
+${renderLicenseSection(data.license)}`;
 }
 
 module.exports = generateMarkdown;
